611289ce59e735ab3ebe95515ee2e2b6bb730425
heloisa.f.santos10@aluno.senai.br <heloisa.f.santos10@aluno.senai.br>
2026-07-20 20:04:24 -0300

    alteração

--- ts/aluno.js
+++ ts/aluno.js
@@ -1,18 +1,21 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Aluno = void 0;
-class Aluno {
+export class Aluno {
+
     constructor(id, nome, idade, curso) {
+
         this.id = id;
         this.nome = nome;
         this.idade = idade;
         this.curso = curso;
+
     }
+
     exibirDados() {
+
         console.log("ID: " + this.id);
         console.log("Nome: " + this.nome);
         console.log("Idade: " + this.idade);
         console.log("Curso: " + this.curso);
+
     }
+
 }
-exports.Aluno = Aluno;
