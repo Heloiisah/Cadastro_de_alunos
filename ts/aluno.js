914d0d97ca46458d4e307da34b3663f2dad66d9c
Heloisa <heloisa.f.santos@aluno.senai.br>
2026-07-20 20:58:07 -0300

    alterações

--- ts/aluno.js
+++ ts/aluno.js
@@ -1,21 +1,14 @@
 export class Aluno {
-
     constructor(id, nome, idade, curso) {
-
         this.id = id;
         this.nome = nome;
         this.idade = idade;
         this.curso = curso;
-
     }
-
     exibirDados() {
-
         console.log("ID: " + this.id);
         console.log("Nome: " + this.nome);
         console.log("Idade: " + this.idade);
         console.log("Curso: " + this.curso);
-
     }
-
 }
--- ts/script.js
+++ ts/script.js
@@ -1,149 +1,168 @@
-"use strict";
-
 import { Aluno } from "./aluno.js";
-
 let alunos = [];
-
+// CAMPOS
 let txtId = document.querySelector("#txtId");
 let txtNome = document.querySelector("#txtNome");
 let txtIdade = document.querySelector("#txtIdade");
 let txtCurso = document.querySelector("#txtCurso");
-
+let txtPesquisar = document.querySelector("#txtPesquisar");
+// BOTÕES
 let btnCadastrar = document.querySelector("#btnCadastrar");
 let btnLimpar = document.querySelector("#btnLimpar");
-
+let btnOrdenar = document.querySelector("#btnOrdenar");
+// TABELA
 let tbAlunos = document.querySelector("#tbAlunos");
-
+// CONTROLE DE EDIÇÃO
 let posEditar = -1;
-
+// CARREGA DADOS DO LOCALSTORAGE
+let dados = localStorage.getItem("alunos");
+if (dados != null) {
+    alunos = JSON.parse(dados);
+    listarAlunos();
+}
+// BOTÃO CADASTRAR
 btnCadastrar.onclick = function () {
-
     if (posEditar == -1) {
         cadastrarAluno();
     }
     else {
         salvarEdicao();
     }
-
 };
-
+// BOTÃO LIMPAR
 btnLimpar.onclick = function () {
     limparCampos();
 };
-
-function cadastrarAluno() {
-
-    let aluno = new Aluno(
-        Number(txtId.value),
-        txtNome.value,
-        Number(txtIdade.value),
-        txtCurso.value
-    );
-
-    alunos.push(aluno);
-
+// BOTÃO ORDENAR
+btnOrdenar.onclick = function () {
+    alunos.sort(function (a, b) {
+        return a.nome.localeCompare(b.nome);
+    });
     listarAlunos();
-
+    localStorage.setItem("alunos", JSON.stringify(alunos));
+};
+// PESQUISA
+txtPesquisar.onkeyup = function () {
+    pesquisarAluno();
+};
+// CADASTRAR
+function cadastrarAluno() {
+    if (txtId.value == "" ||
+        txtNome.value == "" ||
+        txtIdade.value == "" ||
+        txtCurso.value == "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+    if (Number(txtIdade.value) <= 0) {
+        alert("Digite uma idade válida!");
+        return;
+    }
+    let existe = alunos.find(function (aluno) {
+        return aluno.id == Number(txtId.value);
+    });
+    if (existe != undefined) {
+        alert("Já existe um aluno com esse ID!");
+        return;
+    }
+    let aluno = new Aluno(Number(txtId.value), txtNome.value, Number(txtIdade.value), txtCurso.value);
+    alunos.push(aluno);
+    localStorage.setItem("alunos", JSON.stringify(alunos));
+    listarAlunos();
     limparCampos();
-
 }
-
-function listarAlunos() {
-
+// LISTAR
+function listarAlunos(lista = alunos) {
     tbAlunos.innerHTML = "";
-
-    for (let i = 0; i < alunos.length; i++) {
-
+    for (let i = 0; i < lista.length; i++) {
         let tr = document.createElement("tr");
-
         let tdId = document.createElement("td");
-        tdId.innerHTML = alunos[i].id.toString();
-
+        tdId.innerHTML = lista[i].id.toString();
         let tdNome = document.createElement("td");
-        tdNome.innerHTML = alunos[i].nome;
-
+        tdNome.innerHTML = lista[i].nome;
         let tdIdade = document.createElement("td");
-        tdIdade.innerHTML = alunos[i].idade.toString();
-
+        tdIdade.innerHTML = lista[i].idade.toString();
         let tdCurso = document.createElement("td");
-        tdCurso.innerHTML = alunos[i].curso;
-
+        tdCurso.innerHTML = lista[i].curso;
         let tdAcoes = document.createElement("td");
-
         let btnEditar = document.createElement("button");
         btnEditar.innerHTML = "Editar";
         btnEditar.className = "btnEditar";
-
         btnEditar.onclick = function () {
-            editarAluno(i);
+            let indice = alunos.indexOf(lista[i]);
+            editarAluno(indice);
         };
-
         let btnExcluir = document.createElement("button");
         btnExcluir.innerHTML = "Excluir";
         btnExcluir.className = "btnExcluir";
-
         btnExcluir.onclick = function () {
-            excluirAluno(i);
+            let indice = alunos.indexOf(lista[i]);
+            excluirAluno(indice);
         };
-
         tdAcoes.appendChild(btnEditar);
         tdAcoes.appendChild(btnExcluir);
-
         tr.appendChild(tdId);
         tr.appendChild(tdNome);
         tr.appendChild(tdIdade);
         tr.appendChild(tdCurso);
         tr.appendChild(tdAcoes);
-
         tbAlunos.appendChild(tr);
-
     }
-
 }
-
+// PESQUISAR
+function pesquisarAluno() {
+    let texto = txtPesquisar.value.toLowerCase();
+    let listaFiltrada = alunos.filter(function (aluno) {
+        return aluno.nome.toLowerCase().includes(texto);
+    });
+    listarAlunos(listaFiltrada);
+}
+// EDITAR
 function editarAluno(pos) {
-
     txtId.value = alunos[pos].id.toString();
     txtNome.value = alunos[pos].nome;
     txtIdade.value = alunos[pos].idade.toString();
     txtCurso.value = alunos[pos].curso;
-
     posEditar = pos;
-
     btnCadastrar.innerHTML = "Salvar";
-
 }
-
+// SALVAR
 function salvarEdicao() {
-
+    if (txtId.value == "" ||
+        txtNome.value == "" ||
+        txtIdade.value == "" ||
+        txtCurso.value == "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+    for (let i = 0; i < alunos.length; i++) {
+        if (i != posEditar && alunos[i].id == Number(txtId.value)) {
+            alert("Já existe um aluno com esse ID!");
+            return;
+        }
+    }
     alunos[posEditar].id = Number(txtId.value);
     alunos[posEditar].nome = txtNome.value;
     alunos[posEditar].idade = Number(txtIdade.value);
     alunos[posEditar].curso = txtCurso.value;
-
+    localStorage.setItem("alunos", JSON.stringify(alunos));
     listarAlunos();
-
     limparCampos();
-
 }
-
+// EXCLUIR
 function excluirAluno(pos) {
-
-    alunos.splice(pos, 1);
-
-    listarAlunos();
-
+    if (confirm("Deseja realmente excluir este aluno?")) {
+        alunos.splice(pos, 1);
+        localStorage.setItem("alunos", JSON.stringify(alunos));
+        listarAlunos();
+    }
 }
-
+// LIMPAR
 function limparCampos() {
-
     txtId.value = "";
     txtNome.value = "";
     txtIdade.value = "";
     txtCurso.value = "";
-
     posEditar = -1;
-
     btnCadastrar.innerHTML = "Cadastrar";
-
-}  
+}
